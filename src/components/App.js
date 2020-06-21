0ea1da39da45f675fe615f14e4c374afbdea209b
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-06-21 15:04:24 +0200

    Adding the components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import ConnectedTodos from './Todos';
+import ConnectedGoals from './Goals';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 
-function App() {
-  return <div>Hello</div>;
+class App extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    //Fetching the data from the database.
+    dispatch(handleInitialData());
+
+    //Calling forceUpdate to cause a re-render even though is an antipattern.
+    //Typically one call setState to cause a re-render, but
+    //we don't have a state in this case.
+  }
+
+  render() {
+    const { loading } = this.props;
+    if (loading === true) {
+      return <h3>Loading</h3>;
+    }
+    return (
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading,
+}))(App);
